fdb9249f98d2b4eb64b13c8243b7a5f093bfb540
Marcohau20 <haupech2@gmail.com>
2025-01-30 16:01:26 -0500

    Agregar archivos de proyecto.

---
+++ Biblioteca/Context/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Biblioteca.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+
+namespace Biblioteca.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario
+                {
+                    PkUsuario = 1,
+                    Nombre = "Juan",
+                    UserName = "Salmeroncito",
+                    Password = "salme1",
+                    FkRol = 1
+                });
+            //Inserta tabla Rol
+            modelBuilder.Entity<Rol>().HasData(
+                new Rol
+                {
+
+                    PkRol = 1,
+                    Nombre = "Admin"
+                });
+        }
+
+        }
+}
---
+++ Biblioteca/Migrations/20250130202332_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biblioteca.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    PkRol = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.PkRol);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    PkUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PkRol = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.PkUsuario);
+                    table.ForeignKey(
+                        name: "FK_Usuario_Roles_PkRol",
+                        column: x => x.PkRol,
+                        principalTable: "Roles",
+                        principalColumn: "PkRol",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "PkRol", "Nombre" },
+                values: new object[] { 1, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Usuario",
+                columns: new[] { "PkUsuario", "Nombre", "Password", "PkRol", "UserName" },
+                values: new object[] { 1, "Juan", "salme1", 1, "Salmeroncito" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_PkRol",
+                table: "Usuario",
+                column: "PkRol");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
